9b4f9885bea4df0568fd13f201646ce52abb3e45
Ritvik-Santhosh <ritviksanthosh25@gmail.com>
2025-08-25 22:45:19 +0530

    Add files via upload

---
+++ remainder.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;    
+int main()
+{
+    int x,y,rem;
+    cout<<"Enter the number";
+    cin>>x;
+    cin>>y;
+    rem=x%y;
+    cout<<"The remainder is : "<<rem;
+    return 0;
+
+
+    
+}
